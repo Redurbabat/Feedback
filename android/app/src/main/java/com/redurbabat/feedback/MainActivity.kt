fe5c1a403ca7437e6d385c405d9c35e6db234fb3
red <144563381+Redurbabat@users.noreply.github.com>
2026-09-13 13:35:56 +0200

    android: initialize device identity on startup

--- android/app/src/main/java/com/redurbabat/feedback/MainActivity.kt
+++ android/app/src/main/java/com/redurbabat/feedback/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.redurbabat.feedback.security.DeviceIdentityStore
 import com.redurbabat.feedback.ui.FeedbackApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val identityResult = runCatching {
+            DeviceIdentityStore().loadOrCreate()
+        }
+
         setContent {
-            FeedbackApp()
+            FeedbackApp(
+                identity = identityResult.getOrNull(),
+                identityAvailable = identityResult.isSuccess,
+            )
         }
     }
 }
